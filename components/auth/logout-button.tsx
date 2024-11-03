78795cd1cff2ca2737a52d9ae71d292e2b81d9bc
Pankaj Kumar <pankaj1018+1@gmail.com>
2024-11-04 01:52:32 +0530

    Typescript error fixed for build

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { logout } from "@/actions/logout";
-import { useSearchParams } from "next/navigation";
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 
@@ -13,7 +11,6 @@ interface LogoutButtonProps {
 export const LogoutButton = ({
     children
 }: LogoutButtonProps) => {
-    const searchParams = useSearchParams()
     const router = useRouter();
     const [isPending, startTransition] = useTransition();
     // Get the current URL to use as callbackUrl
@@ -26,7 +23,11 @@ export const LogoutButton = ({
     }
     return (
         <span onClick={onClick} className="cursor-pointer">
-            {children || "Logout"}
+            {isPending ? (
+                <span>Loading...</span> // or a spinner/loading indicator
+            ) : (
+                children || "Logout"
+            )}
         </span>
     )
 }
