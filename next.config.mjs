74a3bf6edba1fb86e3ec9676a7e7552c7adefbde
Pankaj Kumar <44459272+kmrpankaj@users.noreply.github.com>
2025-02-05 03:27:20 +0530

    Updated vercel json for reverse proxy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-          {
-            source: "/api/auth/:path*", // Match all NextAuth API calls
-            destination: "https://sloty.in/api/auth/:path*", // Redirect to main auth domain
-          },
-        ];
+      // If you're just returning CORS headers from the nextauth route,
+  // you can define custom headers like so:
+  async headers() {
+    return [
+      {
+        source: "/api/auth/:path*", // All NextAuth routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
       },
+    ];
+  },
 };
 
 export default nextConfig;
