18aeecfea783eef3fb536a7231e1c969839809a4
Pankaj Kumar <44459272+kmrpankaj@users.noreply.github.com>
2025-02-05 03:27:39 +0530

    updated vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-      // If you're just returning CORS headers from the nextauth route,
-  // you can define custom headers like so:
-  async headers() {
-    return [
-      {
-        source: "/api/auth/:path*", // All NextAuth routes
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" }, 
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
